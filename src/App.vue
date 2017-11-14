655a9c5d599ea5a40f0fccfa586415f7351ec6bc
Leopoldthecoder <Leopoldthecuber@gmail.com>
2017-11-14 11:03:00 +0800

    fix button active bgc

--- src/App.vue
+++ src/App.vue
@@ -236,6 +236,7 @@
 
       getStyleTemplate (data) {
         const colorMap = {
+          '#3a8ee6': 'shade-1',
           '#409eff': 'primary',
           '#53a8ff': 'light-1',
           '#66b1ff': 'light-2',
